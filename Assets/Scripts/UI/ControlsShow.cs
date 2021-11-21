73a18bae2c8a34336f648c514bdb5cbc96cc9fdf
João Cavacos <cavacosdoesgaming@gmail.com>
2021-11-21 11:45:01 +0000

    controls begining

---
+++ Assets/Scripts/UI/ControlsShow.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsShow : MonoBehaviour
+{
+   private Settings settings;
+   private void Start()
+   {
+      settings = GetComponent<Settings>();
+   }
+   
+}
