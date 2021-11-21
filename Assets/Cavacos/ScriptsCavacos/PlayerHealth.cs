e144d269ca363976c6f0a6d55546e090cde64694
Ricardo Teixeira <kika.teixeira2001@gmail.com>
2021-11-21 07:55:50 +0000

    HealthFix

--- Assets/Cavacos/ScriptsCavacos/PlayerHealth.cs
+++ Assets/Cavacos/ScriptsCavacos/PlayerHealth.cs
@@ -10,6 +10,7 @@ namespace Cavacos.ScriptsCavacos
         public GameOver _gameOver;
         public AudioSource fxAS;
         public AudioClip takeDamageSound, dieSound;
+        public ParticleSystem bloodPS, parryPS;
 
         void Start()
         {
@@ -35,7 +36,8 @@ namespace Cavacos.ScriptsCavacos
             }
             else
             {
-                Debug.Log("Reset Meter"); 
+                Debug.Log("Reset Meter");
+                bloodPS.Play();
                 base.TakeDamage(damage);
                 Player.instance.parry.ResetMeter();
                 fxAS.PlayOneShot(takeDamageSound);
